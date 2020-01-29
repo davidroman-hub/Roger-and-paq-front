3534a1c8c858b0988d5929d375939b9e201ac024
davidroman-hub <jobroman83@gmail.com>
2020-01-29 15:45:44 +0100

    Routing pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+
+// Core
+
+import Home from './core/Home'
+
+// User
+
+import Signin from './user/Signin'
+import Signup from './user/Signup'
+
+
 
 
 const Routes = () => { 
   return(
-    <div> hola </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component = { Home }/>
+        <Route path='/signin' exact component = { Signin } /> 
+        <Route path='/signup' exact component = { Signup } /> 
+      </Switch>
+    </BrowserRouter>
   )
 }
 
---
+++ src/core/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Link, withRouter} from 'react-router-dom'
+
+
+
+
+
+const Menu = () => <div> Menu</div>
+
+export default withRouter(Menu)
