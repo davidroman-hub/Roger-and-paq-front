e57d69fc1134a8cd2b62947e6bd1b60682a6f8f3
davidroman-hub <jobroman83@gmail.com>
2020-01-30 13:54:31 +0100

    API

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import Layout from '../core/Layout'
 
-const Signin = () => <div> Sign in</div>
+const Signin = () => (
+    <Layout title = "Sign in" description = "Rogen and Paq Sign in" >
+        ...
+    </Layout>
+)
 
 export default Signin
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,5 +1,12 @@
 import React from 'react'
+//import {API} from '../Config' 
+import Layout from '../core/Layout'
 
-const Signup = () => <div> Sign up</div>
-
+const Signup = () => (
+    <Layout title = "Sign Up" description = "Rogen and Paq Sign up" >
+       
+       {/* {API} */}
+        ...
+    </Layout>
+)
 export default Signup
