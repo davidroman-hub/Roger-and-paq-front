a095e8a487b0a5210279c4e2496939ec7a4a3e59
davidroman-hub <jobroman83@gmail.com>
2020-01-31 12:07:00 +0100

    Adding state handleChange to the input

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,27 +1,49 @@
-import React from 'react'
+import React,{ useState } from 'react'
 //import {API} from '../Config' 
 import Layout from '../core/Layout'
 import './Signup.scss'
 
+
+
 const Signup = () => {
+
+    // State 
     
+    const [values, setValues] = useState ({
+        
+        name:'',
+        email:'',
+        password:'',
+        error:'',
+        success:false
+    });
+
+
+    //funtion to take the state
+
+    const handleChange = name => event=> {
+        setValues({...values, error:false, [name]:event.target.value})
+    }
+
+
+
     const signUpForm = () =>( 
     <form  className='SignUpForm'>
         
         <div className = 'container'>
             <p>
                 <label>
-                     <input placeholder="Nombre:" type="text"  />
+                     <input onChange={handleChange('name')} placeholder="Nombre:" type="text"  />
                 </label>
             </p>
             <p>
                 <label>
-                   <input placeholder="E-mail:" type="email"  />
+                   <input onChange={handleChange('email')} placeholder="E-mail:" type="email"  />
                 </label>
             </p>
             <p>
                 <label>
-                   <input placeholder="Contraseña:" type="password"  />
+                   <input onChange={handleChange('password')} placeholder="Contraseña:" type="password"  />
                 </label>
             </p>
         </div> 
@@ -36,6 +58,7 @@ const Signup = () => {
        
        {/* {API} */}
         {signUpForm()}
+        {JSON.stringify(values)}
     </Layout>)
 }
 export default Signup
