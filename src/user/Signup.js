231009a12fa6cab54be7cf5464fe5156394192db
davidroman-hub <jobroman83@gmail.com>
2020-01-31 11:55:34 +0100

    Adding Sign up form

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,12 +1,41 @@
 import React from 'react'
 //import {API} from '../Config' 
 import Layout from '../core/Layout'
+import './Signup.scss'
 
-const Signup = () => (
+const Signup = () => {
+    
+    const signUpForm = () =>( 
+    <form  className='SignUpForm'>
+        
+        <div className = 'container'>
+            <p>
+                <label>
+                     <input placeholder="Nombre:" type="text"  />
+                </label>
+            </p>
+            <p>
+                <label>
+                   <input placeholder="E-mail:" type="email"  />
+                </label>
+            </p>
+            <p>
+                <label>
+                   <input placeholder="Contraseña:" type="password"  />
+                </label>
+            </p>
+        </div> 
+            <button >Submit</button> 
+    </form>
+    
+    )
+
+
+    return(
     <Layout title = "Sign Up" description = "Rogen and Paq Sign up" >
        
        {/* {API} */}
-        ...
-    </Layout>
-)
+        {signUpForm()}
+    </Layout>)
+}
 export default Signup
