dd8102afa1583905edd4c50e5a64a54c939f8b7a
davidroman-hub <jobroman83@gmail.com>
2020-03-02 14:11:44 +0100

    profile component showing user id in the console

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,13 +1,40 @@
 
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import Layout from '../core/Layout'
 import {isAuth} from '../auth/index'
 import {Link} from 'react-router-dom'
+import {read, update, updateUser} from './apiUser'
 
-const Profile = () => { 
+const Profile = ({match}) => { 
+
+const [values, setValues] = useState ({
+    name:'',
+    email:'',
+    password:'',
+    error:false,
+    success:false
+})
+
+const {name,email,password,error,success} =values
+
+const init = (userId) => {
+    console.log(userId)
+}
+
+useEffect(() => {
+    init(match.params.userId)
+})
 
 return (
-    <h1> User profile</h1>
+    // <h1> User profile</h1>
+
+    <Layout title='Perfil del usuario'
+    description='Actualiza tu Informacion'
+    className='container-fluid'
+    >
+        <h2>Actualiza usuario</h2>
+
+    </Layout>
     )
 
 }
